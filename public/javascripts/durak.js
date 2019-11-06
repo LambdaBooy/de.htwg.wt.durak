6637f03e16e14cdf6d1ba552ab14ba0f45522469
Stephan Perren <stephan.perren@htwg-konstanz.de>
2019-11-06 11:16:42 +0100

    WIP

---
+++ public/javascripts/durak.js
@@ -0,0 +1,20 @@
+function handCardDragHandler(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    console.log("drag detected! " + ev.target.id);
+}
+
+function handleHandCardsOnDrop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    if (!ev.target.id.includes("handCard")) {
+        ev.target.appendChild(document.getElementById(data));
+    } else {
+        alert("Bisch du dumm?");
+    }
+    console.log(data);
+    console.log(ev.target.id);
+}
+
+function enableDrop(ev) {
+    ev.preventDefault();
+}
